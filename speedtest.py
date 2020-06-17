a78a46f9216622ba97d1120c554636a2abe0d218
user.name <you@example.com>
2020-06-17 20:01:47 +0300

    17

--- speedtest.py
+++ speedtest.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from skimage import io
 from time import time
 import lycon
+import numpy as np
 
 
 print("Hello!")
@@ -13,14 +14,17 @@ for x in sorted(path.rglob('*.png'))[:1]:
     img = io.imread(x)
     io.imsave(root / 'a.jpg', img)
 
+img1 = io.imread(root / 'a.jpg')
+img2 = lycon.load(str(root / 'a.jpg'))
+print(img1.dtype, img2.dtype)
+print(np.sum(np.abs(img1 - img2)))
+
 begin = time()
 
 sintel_root = Path.cwd().parent.parent.parent
 for x in sorted(path.rglob('*.png'))[:2]:
-    img1 = io.imread(x)[:10, :10]
+    #img1 = io.imread(x)[:10, :10]
     img2 = lycon.load(str(x))[:10, :10]
-    print(img1.dtype, img2.dtype)
-    print(img1 - img2)
 
     #print(x.relative_to(sintel_root))
 
