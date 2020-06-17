ae83ab22e0f22ad6a4b19c055f8c4af46eacbb0f
user.name <you@example.com>
2020-06-18 01:08:02 +0300

    30

--- models/correlation_package/setup.py
+++ models/correlation_package/setup.py
@@ -8,13 +8,11 @@ from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 cxx_args = ['-std=c++11']
 
 nvcc_args = [
-    '-gencode', 'arch=compute_37,code=sm_37',
     '-gencode', 'arch=compute_50,code=sm_50',
     '-gencode', 'arch=compute_52,code=sm_52',
     '-gencode', 'arch=compute_60,code=sm_60',
     '-gencode', 'arch=compute_61,code=sm_61',
-    '-gencode', 'arch=compute_61,code=compute_61',
-    '-ccbin', '/usr/bin/gcc'
+    '-gencode', 'arch=compute_61,code=compute_61'
 ]
 
 setup(
