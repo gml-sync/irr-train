0cf3032f6c9c29f85a4750de2747e6c282c47534
user.name <you@example.com>
2020-06-17 20:46:05 +0300

    20

--- speedtest.py
+++ speedtest.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import skimage
 from skimage import io
 from time import time
 import lycon
@@ -13,15 +14,19 @@ print("cwd:", path)
 sintel_root = Path.cwd().parent.parent.parent
 
 begin = time()
-for x in sorted(path.rglob('*.png')):
+for x in sorted(path.rglob('*.png'))[:10]:
     #img1 = io.imread(x)[:10, :10]
     img2 = lycon.load(str(x))
+    h, w = img2.shape[:2]
+    resized = lycon.resize(img2, width=w // 2, height=h // 2, interpolation=lycon.Interpolation.NEAREST)
 
 print('Lycon spent {:.4f}'.format(time() - begin))
 
 begin = time()
-for x in sorted(path.rglob('*.png')):
+for x in sorted(path.rglob('*.png'))[:10]:
     img1 = io.imread(x)
+    h, w = img1.shape[:2]
+    resized = skimage.transform.resize(img1, (h // 2, w // 2))
     #img2 = lycon.load(str(x))
 
 print('Skimage spent {:.4f}'.format(time() - begin))
