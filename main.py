6be22e7bdedc098e2918fbd1a91c5b8dc3f01815
user.name <you@example.com>
2020-06-17 15:02:12 +0300

    Create exp

--- main.py
+++ main.py
@@ -13,14 +13,14 @@ import torch
 
 def main():
 
-    # Change working directory    
+    # Change working directory
     os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
-    # Parse commandline arguments    
+    # Parse commandline arguments
     args = commandline.setup_logging_and_parse_arguments(blocktitle="Commandline Arguments")
 
-    # Set random seed, possibly on Cuda    
-    config.configure_random_seed(args)    
+    # Set random seed, possibly on Cuda
+    config.configure_random_seed(args)
 
     # DataLoader
     train_loader, validation_loader, inference_loader = config.configure_data_loaders(args)
@@ -32,19 +32,19 @@ def main():
     # Configure data augmentation
     training_augmentation, validation_augmentation = config.configure_runtime_augmentations(args)
 
-    # Configure model and loss    
+    # Configure model and loss
     model_and_loss = config.configure_model_and_loss(args)
 
-    # Resume from checkpoint if available    
+    # Resume from checkpoint if available
     checkpoint_saver, checkpoint_stats = config.configure_checkpoint_saver(args, model_and_loss)
 
-    # Checkpoint and save directory    
+    # Checkpoint and save directory
     with logger.LoggingBlock("Save Directory", emph=True):
         logging.info("Save directory: %s" % args.save)
         if not os.path.exists(args.save):
             os.makedirs(args.save)
 
-    # # Multi-GPU automation    
+    # # Multi-GPU automation
     # with logger.LoggingBlock("Multi GPU", emph=True):
     #     if torch.cuda.device_count() > 1:
     #         logging.info("Let's use %d GPUs!" % torch.cuda.device_count())
@@ -52,11 +52,11 @@ def main():
     #     else:
     #         logging.info("Let's use %d GPU!" % torch.cuda.device_count())
 
-    
-    # Configure optimizer    
+
+    # Configure optimizer
     optimizer = config.configure_optimizer(args, model_and_loss)
-    
-    # Configure learning rate    
+
+    # Configure learning rate
     lr_scheduler = config.configure_lr_scheduler(args, optimizer)
 
     # If this is just an evaluation: overwrite savers and epochs
@@ -68,11 +68,11 @@ def main():
         optimizer = None
         lr_scheduler = None
 
-    # Cuda optimization    
+    # Cuda optimization
     if args.cuda:
         torch.backends.cudnn.benchmark = True
 
-    # Kickoff training, validation and/or testing    
+    # Kickoff training, validation and/or testing
     return runtime.exec_runtime(
         args,
         checkpoint_saver=checkpoint_saver,
@@ -86,4 +86,5 @@ def main():
         validation_augmentation=validation_augmentation)
 
 if __name__ == "__main__":
+    print("Hello world!")
     main()
