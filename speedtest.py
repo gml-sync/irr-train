3abed77b63ea5a6cfcaf858a6f7f1aaa37e0bbca
user.name <you@example.com>
2020-06-17 18:23:18 +0300

    6

--- speedtest.py
+++ speedtest.py
@@ -3,3 +3,6 @@ from pathlib import Path
 print("Hello!")
 path = Path.cwd().parent.parent.parent / 'sintelall/MPI-Sintel-complete/training/clean'
 print("cwd:", path)
+
+for x in path..rglob('*.png'):
+    print(x)
