2a7866d06301a79108f92e07abc14ce5b72907c6
user.name <you@example.com>
2020-06-17 19:07:28 +0300

    10

--- speedtest.py
+++ speedtest.py
@@ -6,8 +6,11 @@ print("Hello!")
 path = Path.cwd().parent.parent.parent / 'sintelall/MPI-Sintel-complete/training/clean'
 print("cwd:", path)
 
+begin = time()
 
 sintel_root = Path.cwd().parent.parent.parent
-for x in path.rglob('*.png'):
-    pass
+for x in sorted(path.rglob('*.png'))[:10]:
+    img = io.imread(x)
     #print(x.relative_to(sintel_root))
+
+print('Spent {:.2f}'.format(time() - begin))
