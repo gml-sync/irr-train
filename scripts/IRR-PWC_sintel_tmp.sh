de38377d1af1bdd4c345d0b2ce39104a657c6c73
gml-sync <example@gmail.com>
2020-09-28 09:19:31 +0300

    4

--- scripts/IRR-PWC_sintel_tmp.sh
+++ scripts/IRR-PWC_sintel_tmp.sh
@@ -1,21 +1,3 @@
-#!/bin/bash
-
-# experiments and datasets meta
-EXPERIMENTS_HOME="experiments"
-
-# datasets
-SINTEL_HOME="$CONDA/datasets/irrpwc/3.Sintel/MPI-Sintel-complete/"
-
-# model and checkpoint
-MODEL=IRR_PWC
-EVAL_LOSS=MultiScaleEPE_PWC_Bi_Occ_upsample_Sintel
-CHECKPOINT="$EXPERIMENTS_HOME/$MODEL-Sintel-v1/checkpoint_latest.ckpt"
-SIZE_OF_BATCH=4
-
-# save path
-TIME=$(date +"%Y%m%d-%H%M%S")
-SAVE_PATH="$EXPERIMENTS_HOME/$MODEL-Sintel-v1"
-
 # training configuration
 python ../main.py \
 --batch_size=$SIZE_OF_BATCH \
@@ -23,23 +5,23 @@ python ../main.py \
 --checkpoint=$CHECKPOINT \
 --lr_scheduler=MultiStepLR \
 --lr_scheduler_gamma=0.5 \
---lr_scheduler_milestones="[258, 302, 346, 368, 374, 379, 401, 423, 445, 467]" \
+--lr_scheduler_milestones="[687, 775, 863, 908, 919, 930, 974, 1018, 1062, 1106]" \
 --model=$MODEL \
 --num_workers=4 \
 --optimizer=Adam \
---optimizer_lr=1.5e-05 \
+--optimizer_lr=1e-05 \
 --optimizer_weight_decay=4e-4 \
 --save=$SAVE_PATH \
---start_epoch=160 \
---total_epochs=489 \
+--start_epoch=490 \
+--total_epochs=1150 \
 --training_augmentation=RandomAffineFlowOccSintel \
 --training_augmentation_crop="[384,768]" \
---training_dataset=SintelTrainingCombTrain \
+--training_dataset=SintelTrainingFinalTrain \
 --training_dataset_photometric_augmentations=True \
 --training_dataset_root=$SINTEL_HOME \
 --training_key=total_loss \
 --training_loss=$EVAL_LOSS \
---validation_dataset=SintelTrainingCombValid  \
+--validation_dataset=SintelTrainingFinalValid  \
 --validation_dataset_photometric_augmentations=False \
 --validation_dataset_root=$SINTEL_HOME \
 --validation_key=epe \
